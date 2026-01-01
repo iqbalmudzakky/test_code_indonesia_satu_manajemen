0519e47855da41afea173c0e2ec4dad345f3bea2
iqbalmudzakky <iqbalmudzakky@gmail.com>
2026-01-01 13:04:02 +0700

    feat: enhance user management by adding unique name validation and auto-incrementing application number

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,36 +2,30 @@ const UserModel = require("../models/UserModel");
 
 module.exports = class UserController {
   static async addUser(req, res) {
-    await run().catch(console.dir);
     try {
       const { nama, tempatLahir, jenisKelamin, alamat } = req.body;
-
-      // handle noAplikasi
-      const userCount = await UserModel.countUsers();
-      let noAplikasi = "";
-      if (userCount === 0) {
-        noAplikasi = 1;
-      } else {
-        noAplikasi = userCount + 1;
-      }
-
-      const summaryScore = 0;
-      const riskLevel = "";
       const tanggalLahir = new Date(req.body.tanggalLahir);
       const kodePos = Number(req.body.kodePos);
       const newUser = {
-        noAplikasi,
         nama,
         tempatLahir,
         tanggalLahir,
         jenisKelamin,
         alamat,
         kodePos,
-        summaryScore,
-        riskLevel,
       };
+
+      const result = await UserModel.addUser(newUser);
+      res.status(201).json({
+        message: "Berhasil menambahkan user",
+      });
     } catch (err) {
       console.log("🚀 ~ addUser ~ err:", err);
+      if (err.status === 400) {
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
     }
   }
 };
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -5,8 +5,36 @@ module.exports = class UserModel {
     return db.collection("users");
   }
 
-  static async countUsers() {
+  static async getUserByNama(nama) {
     const collection = this.getCollection();
-    return await collection.countDocuments();
+    const user = await collection.findOne({ nama });
+    return user;
+  }
+
+  static async addUser(userData) {
+    const collection = this.getCollection();
+
+    // handle unique nama
+    const existingUser = await this.getUserByNama(userData.nama);
+    if (existingUser) {
+      throw { status: 400, message: "Nama sudah terdaftar" };
+    }
+
+    // handle noAplikasi auto increment
+    const user = await collection
+      .find()
+      .sort({ noAplikasi: -1 })
+      .limit(1)
+      .toArray();
+    const noAplikasi = user.length > 0 ? user[0].noAplikasi + 1 : 1;
+    const summaryScore = 0;
+    const riskLevel = "";
+
+    userData.noAplikasi = noAplikasi;
+    userData.summaryScore = summaryScore;
+    userData.riskLevel = riskLevel;
+
+    const result = await collection.insertOne(userData);
+    return result;
   }
 };
